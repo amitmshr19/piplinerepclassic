9bf2ed95d239a41df14d94b8a28e22cbbb5c0f31
amitmshr19 <amitmshr19@gmail.com>
2025-08-24 23:44:36 +0530

    1rg

--- provider.tf
+++ provider.tf
@@ -1,4 +1,14 @@
-provider "azurerm" {
-    features {}
-    subscription id = "d9291a0a-9274-4334-a1b7-210c7a5eb2c6"
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "4.41.0"
+    }
+  }    
+}
+provider {
+    feature {}
+    subscription_id = "d9291a0a-9274-4334-a1b7-210c7a5eb2c6"
+
+    
 }
---
+++ rg.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "rgclassic" {
+  name     = "rg_classic"
+  location = "West Europe"
+}
